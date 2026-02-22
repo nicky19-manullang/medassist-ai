a15e32b6ec731fdf29ec45edbd0d3f7945340e2d
Nicky <nickysimanullang123@gmail.com>
2026-02-22 14:16:29 +0700

    k

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ import models
 import json
 import re
 import hashlib
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
 
 # =========================
 # INITIAL SETUP

350da56645e6ef58a5af2dc96d398fd8a5b51a9b
Nicky <nickysimanullang123@gmail.com>
2026-02-22 13:04:32 +0700

    requairement

--- database.py
+++ database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:@localhost:3307/medassist_ai"
+DATABASE_URL = "mysql://root:AOYkwnNqzkfDjGDMSKVpbQOWaYCWqJiZ@mysql.railway.internal:3306/railway"
+# DATABASE_URL = "mysql+pymysql://root:@localhost:3307/medassist_ai"
+
 
 engine = create_engine(DATABASE_URL)
 

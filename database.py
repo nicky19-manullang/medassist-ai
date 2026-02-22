debc1e014b80ffcc483439dae5403c2a474bd9b9
Nicky <nickysimanullang123@gmail.com>
2026-02-22 13:41:47 +0700

    test

--- database.py
+++ database.py
@@ -1,13 +1,25 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:AOYkwnNqzkfDjGDMSKVpbQOWaYCWqJiZ@interchange.proxy.rlwy.net:46219/railway"
+# local
 # DATABASE_URL = "mysql+pymysql://root:@localhost:3307/medassist_ai"
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL not set!")
+
+# Tambahkan driver jika Railway masih mysql://
+if DATABASE_URL.startswith("mysql://"):
+    DATABASE_URL = DATABASE_URL.replace("mysql://", "mysql+pymysql://", 1)
 
 engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
